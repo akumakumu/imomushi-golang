1819d5d484c249e84f26c7cf469632f9675f7033
Yy <akumakumu@gmail.com>
2024-03-11 16:01:10 +0700

    learn data type

---
+++ data-type.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Boolean
+	// var b1 bool = true
+	// var b2 = true
+	// var b3 bool
+	// b4 := true
+
+	// Integer
+	// int (Signed) store both positive and negative value
+	// uint (Unsigned) store only non-negative
+	// var x int = 100
+	// var y uint = 200
+
+	// Float
+	// there is float32 and float64
+	// var a float32 = 3.2e+32
+
+	// String
+	// just string, what did you expect ?
+	var a string = "hello"
+
+	fmt.Println(a)
+}
